f8b0ed9c04dfe8f01f338d6ec875a8c42ac033ef
Apicalmed-eng <miceremwirigi@gmail.com>
2025-06-04 13:11:35 +0300

    made db variables more dynamic

--- databases/database.go
+++ databases/database.go
@@ -8,18 +8,17 @@ import (
 	"gorm.io/gorm"
 )
 
-func StartDatabase() (db *gorm.DB){
+func StartDatabase() (db *gorm.DB) {
 	log.Println("Starting Database ...")
-	db_name, db_pass, err := LoadConfig()
+	_, db_host, db_user, db_pass, db_name, db_ssl, db_port, err := LoadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	dsn := fmt.Sprintf(`host=localhost user=mqs_user
-		password=%s dbname=%s port=5432 sslmode= disable TimeZone=Africa/Nairobi`, db_pass, db_name)
+	dsn := fmt.Sprintf(`host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Africa/Nairobi`,
+		db_host, db_user, db_pass, db_name, db_port, db_ssl)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err == nil {
 		log.Println("Successfully Started Database")
-
 	} else {
 		log.Fatal(err)
 	}
